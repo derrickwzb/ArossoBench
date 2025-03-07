421ec7ce35c6090dc123a62bbcd6149f158f4d40
derrick <derrick992110@gmail.com>
2025-03-08 00:37:10 +0800

    setup for r3f

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Sky } from '@react-three/drei'
+// import Ocean from "./Components/Ocean.jsx"
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <directionalLight color="red" position={ [1, 2, 3] } intensity={ 2.5 }/>
+
+      <Sky sunPosition={[1, 0.1, 1]}/>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
     </>
   )
 }
 
-export default App
+ 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {  createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+// import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
   <StrictMode>
-    <App />
+    <Canvas
+          camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [52, 7, 12],
+        }}
+        ><App />
+
+    </Canvas> 
   </StrictMode>,
 )
